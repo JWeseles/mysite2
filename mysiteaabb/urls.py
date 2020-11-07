c5ca1e13356ce5cab3536edbe5c0a635c74da66f
JWeseles <jweseles_al@hotmail.com>
2020-11-07 18:34:55 -0300

    Alterações em tests e urls de mysiteaabb

--- mysiteaabb/urls.py
+++ mysiteaabb/urls.py
@@ -1,3 +1,6 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+
 """mysiteaabb URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -13,10 +16,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+'''
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
+]
+'''
+
+urlpatterns = [
+    url(r'^polls/', include('polls.urls')),
+    url(r'^admin/', admin.site.urls),
 ]
